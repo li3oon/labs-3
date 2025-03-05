17a0dfd11ad8919e81670fce1675c9beb678203e
li3oon <lizochkamelnikova8002@gmail.com>
2025-03-05 16:56:34 +0500

    присоединение бд к приложению

--- WpfApp1/obj/Debug/WpfApp1_Content.g.i.cs
+++ WpfApp1/obj/Debug/WpfApp1_Content.g.i.cs
@@ -8,6 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("1/3.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("basemodel.context.tt")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("basemodel.tt")]
 
 
